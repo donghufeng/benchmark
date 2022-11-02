05404fa797a1e2247858177d3130770a13bf4047
donghufeng <xuxusheng2@huawei.com>
2022-11-02 22:18:06 +0800

    random pqc

--- benchmark/task_preparation/random_circuit.py
+++ benchmark/task_preparation/random_circuit.py
@@ -14,7 +14,12 @@
 # ============================================================================
 """Generate random circuit."""
 
+import copy
+
 from mindquantum.utils import random_circuit
+from mindquantum.core.circuit import Circuit
+from mindquantum.core.gates import ParameterGate
+from mindquantum.core.parameterresolver import ParameterResolver
 from benchmark import get_config, get_y, SEED
 
 
@@ -25,7 +30,38 @@ def generate_random_circuit(n_qubits):
     qubit_min = config['qubit_min']
     gate_num_max = config['gate_num_max']
     gate_num_min = config['gate_num_min']
-    if not (qubit_min<=n_qubits<=qubit_max):
-        raise ValueError(f"for {task_name}, qubits should be in [{qubit_min}, {qubit_max}]")
-    gate_num = get_y(n_qubits, qubit_max, gate_num_min, qubit_min, gate_num_max)
+    if not (qubit_min <= n_qubits <= qubit_max):
+        raise ValueError(
+            f"for {task_name}, qubits should be in [{qubit_min}, {qubit_max}]")
+    gate_num = get_y(n_qubits, qubit_max, gate_num_min, qubit_min,
+                     gate_num_max)
     return random_circuit(n_qubits, gate_num, seed=SEED)
+
+
+def generate_random_pqc(n_qubits):
+    task_name = 'random_circuit_gradient'
+    config = get_config(task_name)
+    qubit_max = config['qubit_max']
+    qubit_min = config['qubit_min']
+    gate_num_max = config['gate_num_max']
+    gate_num_min = config['gate_num_min']
+    if not (qubit_min <= n_qubits <= qubit_max):
+        raise ValueError(
+            f"for {task_name}, qubits should be in [{qubit_min}, {qubit_max}]")
+    gate_num = get_y(n_qubits, qubit_max, gate_num_min, qubit_min,
+                     gate_num_max)
+    circ = random_circuit(n_qubits, gate_num, seed=SEED)
+    pqc = Circuit()
+    n_pr = 0
+    gate: ParameterGate
+    for gate in circ:
+        if isinstance(gate, ParameterGate):
+            n_coeff = gate.get_parameters()
+            new_pr = []
+            for i in n_coeff:
+                new_pr.append(ParameterResolver(f'p{n_pr}'))
+                n_pr += 1
+            pqc += gate(*new_pr).on(gate.obj_qubits, gate.ctrl_qubits)
+        else:
+            pqc += copy.deepcopy(gate)
+    return pqc
